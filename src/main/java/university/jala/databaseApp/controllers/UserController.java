90af3b6d7596ddf742d9b68435573a96dd3cc0cb
VEGETTO <oscar.montana@jala.university>
2024-10-08 18:06:29 -0500

    Verificacion de Correo

--- src/main/java/university/jala/databaseApp/controllers/UserController.java
+++ src/main/java/university/jala/databaseApp/controllers/UserController.java
@@ -1,10 +1,10 @@
 package university.jala.databaseApp.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import university.jala.databaseApp.Dto.UserDto;
-import university.jala.databaseApp.Dto.SaveUserDto;
 import university.jala.databaseApp.services.UserService;
 
 import java.util.List;
@@ -25,10 +25,16 @@ public class UserController {
         return userService.findById(id);
     }
 
-    @PostMapping("/users/save")
-    public UserDto save(@RequestBody SaveUserDto saveUserDto) {
-        return userService.save(saveUserDto);
-    }
+//    @PostMapping("/users/save")
+//    public UserDto save(@RequestBody SaveUserDto saveUserDto) {
+//        return userService.save(saveUserDto);
+//    }
+
+//    @PostMapping("/mail/validate/email")
+//    public ResponseEntity<String> createUser(@RequestBody UserDto userDto) {
+//        emailService.sendVerificationEmail(userDto.getEmail());
+//        return new ResponseEntity<>("User created successfully. Please check your email to verify your account.", HttpStatus.OK);
+//    }
 
     @PutMapping("/users/update/{id}")
     public UserDto updateUser(@RequestBody UserDto userDto, @PathVariable Long id) {
