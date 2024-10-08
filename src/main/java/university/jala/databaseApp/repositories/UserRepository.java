080823cfdc1c75a9ef7d1a4bdae6a9c89f303eed
JoseHernandez14 <Jose.Hernandez@jala.university>
2024-10-08 17:07:52 -0600

    Task Impementation

--- src/main/java/university/jala/databaseApp/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package university.jala.databaseApp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import university.jala.databaseApp.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
